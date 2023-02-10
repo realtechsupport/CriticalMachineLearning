0df10010ab6dd9e389a3898b4ac0d22e4d12d821
RTS <25514511+realtechsupport@users.noreply.github.com>
2023-02-10 12:24:40 -0500

    Update basics.sh

--- setup/basics.sh
+++ setup/basics.sh
@@ -6,16 +6,14 @@
 #-------------------------------------------
 clear
 echo "WELCOME - basics for the VM install "
-
+# upgrade
 sudo apt-get update
 sudo apt-get upgrade -y
-
-
+# python
 sudo apt-get install python3-dev -y
 sudo apt-get install build-essential python3-dev -y
 sudo apt-get install python3-venv -y
-
-
+# packages
 pip3 install --upgrade pip
 pip3 install --upgrade pillow
 pip3 install numpy
@@ -24,8 +22,7 @@ pip3 install pandas
 pip3 install scipy
 pip3 install scikit-learn
 
-echo "installed python3, python3dev, python3-venv, numpy, scipy, pandas, matplotlib, scikit"
-
+echo "installed python3, python3dev, python3-venv, matplotlib, numpy, pandas, pillow, scikit, scipy"
 echo "hit ctrl d to close this session"
 exit 0
-#end this part by closing the terminal and lanuching a new SSH session
+#end this operation by closing the terminal and lanuching a new SSH session
